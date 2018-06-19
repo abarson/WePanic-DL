c1e40fd0e4539f4fda383b7fc39d9a05d2b408ed
danielberenberg <djberenb@uvm.edu>
2018-06-19 19:55:25 -0400

    redscale_script

---
+++ redscale_it.py
@@ -0,0 +1,39 @@
+"""
+redscale an image
+"""
+
+import sys
+import os
+import argparse
+import numpy as np
+from PIL import Image
+from scipy.misc import toimage
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='redscale an image')
+
+    parser.add_argument('input_image',
+                        help='the image to redscale',
+                        type=str)
+
+    return parser
+
+
+if __name__ == '__main__':
+
+    args = parse_args().parse_args()
+    
+    if not os.path.exists(args.input_image):
+        raise FileNotFoundError('cannot locate %s' % args.input_image)
+
+    
+    im = Image.open(args.input_image)
+    tens = np.array(im)
+    R = tens[:,:,0]
+    
+    name = args.input_image.split('/')[-1].split('.')[0] + '_red.png'
+
+    toimage(R, cmin=0.0, cmax=...).save(name)
+
+
