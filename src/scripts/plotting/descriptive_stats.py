b620fbca4812c1d4ed95f2a60d2bccc882354f51
danielberenberg <djberenb@uvm.edu>
2018-06-26 20:16:13 -0400

    descriptive stats

---
+++ src/scripts/plotting/descriptive_stats.py
@@ -0,0 +1,14 @@
+"""
+report descriptive statistics and generate some plots about the data
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import sys, os
+
+# assumes the most up to date
+wpc_cc = 'wepanic_collated_catalogue.csv'
+
+param_slug = ''
+
+
