5dbc0ed7fe46f8bc029358e517b921b954c78472
dberenberg <djberenb@uvm.edu>
2018-06-28 07:18:46 -0400

    new model

--- src/we_panic_utils/we_panic_utils/nn/models/RegressionModel.py
+++ src/we_panic_utils/we_panic_utils/nn/models/RegressionModel.py
@@ -2,7 +2,7 @@ from keras.layers import Dense, Flatten, Dropout, ZeroPadding3D, Input
 from keras.models import Sequential  # load_model
 from keras.optimizers import Adam,  RMSprop, SGD
 from keras.layers.convolutional import Conv3D, MaxPooling3D
-from keras.layers import BatchNormalization
+from keras.layers import Activation, BatchNormalization
 from keras.models import Model
 
 class RegressionModel():
@@ -84,6 +84,49 @@ class C3D(RegressionModel):
         model.add(Dropout(0.5))
         model.add(Dense(self.output_shape, activation='linear'))
 
+        return model
+
+class CNN_3D_BN_DO(RegressionModel):
+   
+    def __init__(self, input_shape, output_shape, loss=None):
+        RegressionModel.__init__(self, input_shape, output_shape, loss=loss)
+
+    def instantiate(self):
+        return super(CNN_3D_BN_DO, self).instantiate()
+
+    def get_model(self):
+       
+        model = Sequential()
+        
+        model.add(Conv3D(64, kernel_size=(15, 15, 3), 
+                  input_shape=self.input_shape))
+        model.add(BatchNormalization()) 
+        model.add(Activation('relu'))
+        model.add(MaxPooling3D(pool_size=2, strides=(2, 2, 2)))
+
+        model.add(Conv3D(128, kernel_size=(5, 5, 3)))
+        model.add(BatchNormalization()) 
+        model.add(Activation('relu'))
+        model.add(MaxPooling3D(pool_size=2, strides=(2, 2, 2)))
+
+        model.add(Conv3D(256, kernel_size=(3, 3, 3)))
+        model.add(BatchNormalization()) 
+        model.add(Activation('relu'))
+        model.add(Conv3D(256, kernel_size=(3, 3, 3)))
+        model.add(BatchNormalization()) 
+        model.add(Activation('relu'))
+        model.add(Conv3D(256, kernel_size=(3, 3, 3)))
+        model.add(BatchNormalization()) 
+        model.add(Activation('relu'))
+        model.add(MaxPooling3D(pool_size=2, strides=2))
+
+        model.add(Flatten()) 
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.output_shape, activation='linear'))
+        
         return model
 
 class CNN_3D(RegressionModel):
