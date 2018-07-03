cd713f3ba0c9ad87e0e0ece2906fbe03f3303997
danielberenberg <djberenb@uvm.edu>
2018-07-02 20:01:52 -0400

    fix

--- src/scripts/retire/informatics/info_extraction/load_up_wpcc.py
+++ src/scripts/retire/informatics/info_extraction/load_up_wpcc.py
@@ -4,4 +4,4 @@ use this script to debug with we_panic_collated_catalogue.csv
 
 import pandas as pd
 
-WPDATA = pd.read_csv('we_panic_collated_catalogue.csv')
+WPDATA = pd.read_csv('wepanic_collated_catalogue.csv')
